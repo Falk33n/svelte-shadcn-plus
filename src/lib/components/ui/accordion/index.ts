233d2b57b3ffa26c54e9dda64b412341367fc175
Falk33n <tim.falk00@gmail.com>
2025-02-15 21:15:53 +0100

    fix: remove accordion

--- src/lib/components/ui/accordion/index.ts
+++
@@ -1,8 +0,0 @@
-import { Accordion as AccordionPrimitive } from 'bits-ui';
-
-export {
-	default as AccordionItem,
-	type AccordionItemProps,
-} from './AccordionItem.svelte';
-
-export const Accordion = AccordionPrimitive.Root;
