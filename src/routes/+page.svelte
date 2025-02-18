64ded11403a59211fb6b44a623e50a23e7dc877b
Falk33n <tim.falk00@gmail.com>
2025-02-18 16:21:16 +0100

    feat: update colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,43 @@
 	import { getMode, setMode } from '$components/ui/mode-watcher';
 </script>
 
-<div class="flex min-h-screen w-full items-center justify-center gap-4">
-	<Button onclick={() => setMode('light')}>Change light</Button>
-	<Button onclick={() => setMode('dark')}>Change dark</Button>
-	<Button onclick={() => setMode('system')}>Change system</Button>
-	<Button onclick={() => console.log(getMode())}>See current mode</Button>
-	<Button variant="destructive">This is a button</Button>
-	<Button variant="ghost">This is a button</Button>
-	<Button variant="link">This is a button</Button>
-	<Button variant="outline">This is a button</Button>
-	<Button variant="secondary">This is a button</Button>
+<div
+	class="flex min-h-screen w-full flex-col items-center justify-center gap-8"
+>
+	<div class="flex items-center gap-2">
+		<Button onclick={() => setMode('light')}>Change light</Button>
+		<Button onclick={() => setMode('dark')}>Change dark</Button>
+		<Button onclick={() => setMode('system')}>Change system</Button>
+		<Button onclick={() => console.log(getMode())}>See current mode</Button>
+	</div>
+	<div class="flex items-center gap-2">
+		<div
+			class="bg-secondary text-secondary-foreground rounded-md px-4 py-1.5 font-medium"
+		>
+			Secondary
+		</div>
+		<div
+			class="bg-accent text-accent-foreground rounded-md px-4 py-1.5 font-medium"
+		>
+			Accent
+		</div>
+		<div
+			class="bg-muted text-muted-foreground rounded-md px-4 py-1.5 font-medium"
+		>
+			Muted
+		</div>
+		<div
+			class="bg-primary text-primary-foreground rounded-md px-4 py-1.5 font-medium"
+		>
+			Primary
+		</div>
+		<div
+			class="bg-destructive text-destructive-foreground rounded-md px-4 py-1.5 font-medium"
+		>
+			Destructive
+		</div>
+		<div class="text-foreground rounded-md border px-4 py-1.5 font-medium">
+			Border
+		</div>
+	</div>
 </div>
