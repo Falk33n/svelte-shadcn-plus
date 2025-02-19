e194529aa7ebaaef4d084a3c94530d51b8dcbd8c
Falk33n <tim.falk00@gmail.com>
2025-02-19 11:25:04 +0100

    feat: add icons and types

---
+++ src/lib/components/ui/icon/TriangleAlertIcon.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import type { IconProps } from '$components/ui/icon';
+	import { cn } from '$utils';
+
+	let {
+		ref = $bindable(null),
+		'class': className,
+		'aria-hidden': ariaHidden,
+		...restProps
+	}: IconProps = $props();
+</script>
+
+<svg
+	bind:this={ref}
+	class={className ? cn(className) : undefined}
+	aria-hidden={ariaHidden !== undefined ? ariaHidden : true}
+	xmlns="http://www.w3.org/2000/svg"
+	width="24"
+	height="24"
+	viewBox="0 0 24 24"
+	fill="none"
+	stroke="currentColor"
+	stroke-width="2"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	{...restProps}
+>
+	<path
+		d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3"
+	/>
+	<path d="M12 9v4" />
+	<path d="M12 17h.01" />
+</svg>
