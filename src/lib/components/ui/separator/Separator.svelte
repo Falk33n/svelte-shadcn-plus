67c19b5fbcf4aefb522f442cd281c635231b2b4d
Falk33n <tim.falk00@gmail.com>
2025-02-21 11:11:08 +0100

    feat: add separator

---
+++ src/lib/components/ui/separator/Separator.svelte
@@ -0,0 +1,37 @@
+<script
+	lang="ts"
+	module
+>
+	import type {
+		WithClassAsString,
+		WithElementRef,
+		WithoutChildren,
+	} from '$types';
+	import { cn } from '$utils';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	export type SeparatorProps = WithClassAsString<
+		WithElementRef<WithoutChildren<HTMLAttributes<HTMLHRElement>>>
+	> & { orientation?: HTMLAttributes<HTMLHRElement>['aria-orientation'] };
+</script>
+
+<script lang="ts">
+	let {
+		ref = $bindable(null),
+		'class': className,
+		orientation = 'horizontal',
+		'aria-orientation': ariaOrientation = orientation,
+		...restProps
+	}: SeparatorProps = $props();
+</script>
+
+<hr
+	bind:this={ref}
+	class={cn(
+		'bg-border my-4 shrink-0',
+		orientation === 'horizontal' ? 'h-px w-full' : 'h-full w-px',
+		className,
+	)}
+	aria-orientation={ariaOrientation || orientation}
+	{...restProps}
+/>
