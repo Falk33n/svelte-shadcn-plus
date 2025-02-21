a9d5486505f61d0f8762b286194d1caba779f809
Falk33n <tim.falk00@gmail.com>
2025-02-21 15:13:51 +0100

    feat: add label

---
+++ src/lib/components/ui/label/Label.svelte
@@ -0,0 +1,32 @@
+<script
+	lang="ts"
+	module
+>
+	import type { WithClassAsString, WithElementRef } from '$types';
+	import { cn } from '$utils';
+	import type { HTMLLabelAttributes } from 'svelte/elements';
+
+	export type LabelProps = WithClassAsString<
+		WithElementRef<HTMLLabelAttributes>
+	>;
+</script>
+
+<script lang="ts">
+	let {
+		ref = $bindable(null),
+		class: className,
+		children,
+		...restProps
+	}: LabelProps = $props();
+</script>
+
+<label
+	bind:this={ref}
+	class={cn(
+		'leading-none font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+		className,
+	)}
+	{...restProps}
+>
+	{@render children?.()}
+</label>
